31fe323e0c99bf9dd81e5b46509e3a85c81e11f6
videju <77975858+videju@users.noreply.github.com>
2022-04-25 15:28:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+let toggle = document.querySelector('.toggle');
+let menu= document.querySelector('.menu');
+toggle.onclick = function(){
+    menu.classList.toggle('active')
+}
+
+//code for image slider
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+  autoplay:{
+      delay:3000,
+      disableOnInteraction:false,
+  },
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
